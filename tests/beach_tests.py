043bd89f044e5884d985698e8eeb72b9ee0797c6
Michael Williamson <mike@zwobble.org>
2014-01-12 18:08:50 +0000

    Use apt-cacher-ng to speed up apt

--- tests/beach_tests.py
+++ tests/beach_tests.py
@@ -4,6 +4,7 @@ import os
 from nose.tools import istest, nottest, assert_equal
 import peachtree
 import spur
+import starboard
 
 
 _local = spur.LocalShell()
@@ -12,7 +13,19 @@ _local = spur.LocalShell()
 class BeachTests(object):
     def _start_vm(self):
         provider = peachtree.qemu_provider()
-        return provider.start(self.image_name)
+        machine = provider.start(self.image_name)
+        try:
+            root_shell = machine.root_shell()
+            hostname = starboard.find_local_hostname()
+            # TODO: verify apt-cacher-ng is running
+            # TODO: verify that caching is working correctly
+            apt_config = 'Acquire::http::Proxy "http://{0}:3142";\n'.format(hostname)
+            with root_shell.open("/etc/apt/apt.conf.d/01-proxy-cache", "w") as config_file:
+                config_file.write(apt_config)
+            return machine
+        except:
+            machine.destroy()
+            raise
     
     @istest
     def can_use_vm(self):
@@ -21,6 +34,7 @@ class BeachTests(object):
             shell = machine.shell()
             result = shell.run(["echo", "hello"])
             assert_equal("hello\n", result.output)
+    
 
 
 @istest

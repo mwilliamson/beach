8e6267297ad47015ac17c52b7c5f77d8eb0e1848
Michael Williamson <mike@zwobble.org>
2014-04-14 17:16:03 +0100

    Add command for register

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -6,6 +6,7 @@ import spur
 from nose.tools import istest, assert_equal
 
 from . import testing
+import beach
 
 _local = spur.LocalShell()
 
@@ -33,4 +34,22 @@ def can_deploy_temporarily_using_cli():
             process.send_signal(signal.SIGINT)
             process.wait_for_result()
     
-    
+
+@istest
+def can_register_services_using_cli():
+    with tempfile.NamedTemporaryFile() as registry_file:
+        config = {"registry": {"file": registry_file.name}}
+        
+        with tempfile.NamedTemporaryFile() as config_file:
+            json.dump(config, config_file)
+            config_file.flush()
+        
+            _local.run([
+                "beach", "register",
+                "node-0.10", "-ppath=/opt/node-0.10.2/",
+                "-c", config_file.name
+            ])
+            
+        registry = beach.registries.FileRegistry(_local, registry_file.name)
+        service = registry.find_service("node-0.10")
+        assert_equal("/opt/node-0.10.2/", service.provides["path"])

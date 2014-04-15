242c6ed8511d0581e1ca6d3e30a16b346b4a5c3c
Michael Williamson <mike@zwobble.org>
2014-04-15 16:28:15 +0100

    Use registry when deploying using CLI

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -13,26 +13,34 @@ _local = spur.LocalShell()
 
 @istest
 def can_deploy_temporarily_using_cli():
-    app_path = testing.example_app_path("just-a-script")
+    app_path = testing.example_app_path("script-with-dependency")
     # TODO: allow integer parameters
     params = {"port": "58080"}
-    config = {"params": params}
     
-    with tempfile.NamedTemporaryFile() as config_file:
-        json.dump(config, config_file)
-        config_file.flush()
-    
-        process = _local.spawn([
-            "beach", "deploy",
-            app_path,
-            "-c", config_file.name
-        ])
-        try:
-            response = testing.retry_http_get("http://localhost:58080", timeout=1)
-            assert_equal("Hello", response.text)
-        finally:
-            process.send_signal(signal.SIGINT)
-            process.wait_for_result()
+    with tempfile.NamedTemporaryFile() as registry_file:
+        registry = beach.registries.FileRegistry(_local, registry_file.name)
+        registry.register("message", provides={"value": "Sailing to Philadelphia"})
+        
+        config = {
+            "registry": {"path": registry_file.name},
+            "params": params,
+        }
+        
+        with tempfile.NamedTemporaryFile() as config_file:
+            json.dump(config, config_file)
+            config_file.flush()
+        
+            process = _local.spawn([
+                "beach", "deploy",
+                app_path,
+                "-c", config_file.name
+            ])
+            try:
+                response = testing.retry_http_get("http://localhost:58080", timeout=1)
+                assert_equal("Sailing to Philadelphia", response.text)
+            finally:
+                process.send_signal(signal.SIGINT)
+                process.wait_for_result()
 
 
 # TODO: test CLI command line parsing separately
